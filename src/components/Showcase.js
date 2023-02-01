c08620b3963a58c7b02870fd8506a4a84b8e317a
Juan Roma <romajuanmanuel@gmail.com>
2023-02-01 20:16:07 -0300

    sape

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,60 +1,53 @@
 import React from "react";
+import web3d from "../img/web3d.jpg";
+import "../styles/Showcase.css";
 
-function Showcase(){
-    return (
-        <section class="showcase">
-<div>
-  <picture>
-    <source
-      media="(min-width: 768px)"
-      srcset="./assets/images/image-web-3-desktop.jpg"
-    />
-    <img src="./assets/images/image-web-3-mobile.jpg" alt="" />
-  </picture>
+function Showcase() {
+  return (
+    <section className="showcase">
+      <div className="first">
+        <picture>
+          <img className="wew3d" src={web3d} alt="" />
+        </picture>
+        <article className="artFuture">
+          <h1>The Bright Future of Web 3.0?</h1>
+          <div>
+            <p>
+              We dive into the next evolution of the web that claims to put the
+              power of the platforms back into the hands of the people. But is
+              it really fulfilling its promise?
+            </p>
+            <button>Read more</button>
+          </div>
+        </article>
+      </div>
 
-  <article>
-    <h1>The Bright Future of Web 3.0?</h1>
+      <div className="sec">
+        <h2>New</h2>
 
-    <div>
-      <p>
-        We dive into the next evolution of the web that claims to put the
-        power of the platforms back into the hands of the people. But is
-        it really fulfilling its promise?
-      </p>
+        <article className="hola">
+          <h3>Hydrogen VS Electric Cars</h3>
+          <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+        </article>
 
-      <button>Read more</button>
-    </div>
-  </article>
-</div>
+        <article>
+          <h3>The Downsides of AI Artistry</h3>
+          <p>
+            What are the possible adverse effects of on-demand AI image
+            generation?
+          </p>
+        </article>
 
-<div>
-  <h2>New</h2>
-
-  <article>
-    <h3>Hydrogen VS Electric Cars</h3>
-    <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-  </article>
-
-  <article>
-    <h3>The Downsides of AI Artistry</h3>
-    <p>
-      What are the possible adverse effects of on-demand AI image
-      generation?
-    </p>
-  </article>
-
-  <article>
-    <h3>Is VC Funding Drying Up?</h3>
-    <p>
-      Private funding by VC firms is down 50% YOY. We take a look at what
-      that means.
-    </p>
-  </article>
-</div>
-</section>
-
-    );
+        <article>
+          <h3>Is VC Funding Drying Up?</h3>
+          <p>
+            Private funding by VC firms is down 50% YOY. We take a look at what
+            that means.
+          </p>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Showcase;
-
